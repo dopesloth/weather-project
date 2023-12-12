b54992212b3b7b8af66cbbd7d4e8d99d6e859b3b
dopesloth <49575836+dopesloth@users.noreply.github.com>
2023-12-11 22:48:47 -0500

    Added search function with API and updated CSS

--- src/script.js
+++ src/script.js
@@ -1,11 +1,24 @@
-let searchForm = document.querySelector(".search-form");
-console.log(searchForm);
+function refreshTemp(response) {
+  let newTemperature = document.querySelector("#temp-now");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  newTemperature.innerHTML =
+    Math.round(response.data.temperature.current) + "°F";
+}
+
+function searchCity(city) {
+  let apiKey = "c8e21939ob5bfbb2ea332t339f450f8b";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=imperial`;
+  axios.get(apiURL).then(refreshTemp);
+}
 
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
+let searchForm = document.querySelector(".search-form");
 searchForm.addEventListener("submit", searchSubmit);
+
+searchCity("Paris");
